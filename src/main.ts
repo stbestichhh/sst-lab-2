86a1215192fad27b2fb8b01a80c44e44a34d40c4
Kiril Yakymchuk <stbestich@gmail.com>
2024-10-17 18:12:17 +0300

    feat: validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const PORT = 9110;
+  const HOST = 'localhost';
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+  });
 }
 bootstrap();
