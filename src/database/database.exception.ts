8c16fb06b3b652418dddcd60093c856899272e1d
Kiril Yakymchuk <stbestich@gmail.com>
2024-10-17 18:11:18 +0300

    some fixes of database

---
+++ src/database/database.exception.ts
@@ -0,0 +1,7 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class DatabaseException extends HttpException {
+  constructor(message: string) {
+    super({ message, error: 'Database Error', status: HttpStatus.BAD_REQUEST }, HttpStatus.BAD_REQUEST);
+  }
+}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,4 +4,10 @@ import { DatabaseService } from './database.service';
 @Module({
   providers: [DatabaseService],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  public static register(tables: string[]) {
+    const dbService = new DatabaseService();
+    dbService.init(tables);
+    return this;
+  }
+}
