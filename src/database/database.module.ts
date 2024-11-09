6d4beb51787551781942fd2f8f0b868efc6ab598
Kiril Yakymchuk <stbestich@gmail.com>
2024-11-09 11:18:19 +0200

    database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,43 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as process from 'node:process';
+import * as Joi from 'joi';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   providers: [DatabaseService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().hostname().required(),
+        POSTGRES_PORT: Joi.number().port().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
+    }),
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        models: [],
+        autoLoadModels: true,
+        sync: { alter: true, force: false },
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {
-  public static register(tables: string[]) {
-    const dbService = new DatabaseService();
-    dbService.init(tables);
-    return this;
+  public static forFeature(entities: any[]) {
+    return SequelizeModule.forFeature(entities);
   }
 }
